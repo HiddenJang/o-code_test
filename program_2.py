6655e66f41bdb46022f2d880f824c37a61abc9a9
HiddenJang <jangljungl@gmail.com>
2024-03-16 23:15:28 +0400

    comleted

--- program_2.py
+++ program_2.py
@@ -1,27 +1,44 @@
 import numpy as np
 import pandas as pd
-
 import matplotlib
 import matplotlib.pyplot as plt
-import matplotlib.animation as animation
 
 matplotlib.use('QtAgg')
 
 ########## Программа 2 - основное задание ###############
 
-def calculate_distances(vectors_array: list) -> list:
+def calculate_distances(vectors_array: np.ndarray) -> pd.DataFrame:
     """Вычисление Евклидовых расстояний, поиск минимального и максимального расстояний"""
 
-    distances = []
+    distances_df = pd.DataFrame()
+    distances_df["dist_amount"] = []
+    distances_df["distance"] = []
+
+    ## В связи с ограничением по количеству элементов структур данных, возможным видится следующий вариант:
+    ## посчитать количество каждого расстояния округленного до 0.1 (меньшие значения не подлежат отрисовке)
+    ## и потом построить гистограмму распределения на основе сформированных данных
+
     for vector_first in range(0, len(vectors_array)-1):
         for vector_second in range(vector_first+1, len(vectors_array)):
+
+            ## Вычисление значения Евклидова расстояния ##
             point_1 = np.array(vectors_array[vector_first], dtype=np.float_)
             point_2 = np.array(vectors_array[vector_second], dtype=np.float_)
             vectors_square = np.square(point_1 - point_2)
             sum_vectors_square = np.sum(vectors_square)
             euclid_distance_solved = np.sqrt(sum_vectors_square)
 
-            distances.append(euclid_distance_solved)
+            ## Формирование для построения гистограммы дата-фрейма с количеством расстояний,
+            ## округленных до 0.1 (поскольку шаг распределения расстояний на гистограмме задан 0.1).
+            ## Если расстояние, округленное до 0.1 есть в дата-фрейме, то увеличиваем количество на единицу,
+            ## если нет - то добавляем новую строку в дата-фрейм.
+            if distances_df["distance"].isin([round(euclid_distance_solved, 1)]).any():
+                row_df = distances_df[distances_df["distance"] == round(euclid_distance_solved, 1)]
+                row_index = row_df.index.to_list()[0]
+                distances_df.iat[row_index, 0] = row_df.iat[0, 0] + 1
+            else:
+                new_row_df = pd.DataFrame([{"dist_amount": 1, "distance": round(euclid_distance_solved, 1)}])
+                distances_df = distances_df._append(new_row_df, ignore_index=True)
 
             if not vector_first and vector_second:
                 max_distance_vectors_numbers = [vector_first, vector_second]
@@ -45,15 +62,17 @@ def calculate_distances(vectors_array: list) -> list:
           f'максимальное расстояние: {max_distance}, \n'
           f'Номера векторов с минимальным расстоянием: {min_distance_vectors_numbers}, '
           f'минимальное расстояние: {min_distance}')
-    return distances
+    return distances_df
 
 
 if __name__ == '__main__':
     vectors_array = np.genfromtxt('vectors.csv', delimiter=',')
-    distances = calculate_distances(vectors_array)
+    distances_df = calculate_distances(vectors_array)
 
-    ## Построение гистограммы распределения Евклидовых расстояний между различными парами векторов в зависимости от количества сочетаний ##
-    df = pd.DataFrame(distances)
-    plt.hist(df, bins=int(df[0].max()/0.1))
+    ## Построение гистограммы распределения Евклидовых расстояний между различными парами векторов с шагом 0.1
+    plt.bar(distances_df["distance"], distances_df["dist_amount"], width=0.1)
+    plt.xlabel("Величина Евклидова расстояния")
+    plt.ylabel("Количество Евклидовых расстояний")
+    plt.title("Гистограмма распределения Евклидовых расстояний")
     plt.show()
 
